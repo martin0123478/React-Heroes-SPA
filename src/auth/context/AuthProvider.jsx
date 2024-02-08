b124f470549c2719a3f702373b3885fb17c7de96
martin0123478 <martin_agui97@outlook.es>
2024-02-07 21:45:55 -0600

    feat:mantener usuario activo

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -5,17 +5,23 @@ import { types } from '../types/types'
 const initialState = {
     logged: false
 }
+const init = () => {
+    const user = JSON.parse(localStorage.getItem('user'))
+    return {
+        logged: !!user,
+        user: user
+    }
+}
 export const AuthProvider = ({ children }) => {
-    const [state, dispatch] = useReducer(authReducer, initialState)
+    const [state, dispatch] = useReducer(authReducer, initialState, init)
 
     const login = (name = '') => {
+        const user = { id: 'ABC', name }
         const action = {
             type: types.login,
-            payload: {
-                id: 'ABC',
-                name: name
-            }
+            payload: user
         }
+        localStorage.setItem('user', JSON.stringify(user))
         dispatch(action)
     }
     return (
