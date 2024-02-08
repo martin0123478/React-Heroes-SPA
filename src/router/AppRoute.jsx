7b3266f7ca25b9c3719630cc8367694e051b8940
martin0123478 <martin_agui97@outlook.es>
2024-02-07 22:29:08 -0600

    feat: rutas publicas

--- src/router/AppRoute.jsx
+++ src/router/AppRoute.jsx
@@ -4,12 +4,20 @@ import { Navbar } from '../ui'
 import { LoginPage } from '../auth/pages/LoginPage'
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes'
 import { PrivateRoute } from './PrivateRoute'
+import { PublicRoute } from './PublicRoute'
 export const AppRoute = () => {
     return (
         <>
             <Routes>
 
+                <Route
 
+                    path='/login' element={
+                        <PublicRoute>
+                            <LoginPage />
+                        </PublicRoute>
+                    }
+                />
                 <Route path='login' element={<LoginPage />} />
                 <Route path="/*" element={
                     <PrivateRoute>
