eb35803d3913a062cb84066089b586737e3ae3e8
martin0123478 <martin_agui97@outlook.es>
2024-02-10 21:44:38 -0600

    Pruebas en rutas publicas y privadas

---
+++ tests/router/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { render } from "@testing-library/react"
+import { AuthContext } from "../../src/auth/context/AuthContext"
+import { PublicRoute } from "../../src/router/PublicRoute"
+
+describe('Pruebas en private route', () => {
+    test('Debe de mostrar el chidren si  esta autenticado', () => {
+        const contextValue = {
+            logged: false,
+
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <PublicRoute>
+                    <h1>Ruta Privada</h1>
+                </PublicRoute>
+            </AuthContext.Provider>
+        )
+
+        //  screen.debug()
+        expect(screen.getByText('Ruta Privada')).toBeTruthy()
+    })
+})
--- tests/router/PublicRoute.test.jsx
+++ tests/router/PublicRoute.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import { PublicRoute } from "../../src/router/PublicRoute"
 import { AuthContext } from "../../src/auth/context/AuthContext"
+import { MemoryRouter, Route, Routes } from "react-router-dom"
 
 describe('Pruebas en public Route', () => {
     test('Debe de mostrar el chidren si no esta autenticado', () => {
@@ -21,6 +22,30 @@ describe('Pruebas en public Route', () => {
     })
 
     test('should navegar si esta autenticado', () => {
+        const contextValue = {
+            logged: true,
+            user: {
+                name: 'hook',
+                id: 'ABC'
+            }
 
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Ruta Publica</h1>
+                            </PublicRoute>
+                        }
+                        />
+                        <Route path='marvel' element={<h1>Pagina Marvel</h1>} />
+                    </Routes>
+
+                </MemoryRouter>
+            </AuthContext.Provider>
+        )
+        expect(screen.getByText('Pagina Marvel')).toBeTruthy()
     })
 })
